88409417ed06878436f2d74d5e0fb73daba9c562
ROHIT12-bit <reddyrohit823@gmail.com>
2025-12-25 17:49:34 +0530

    Delete api directory

--- api/m3u.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "audio/x-mpegurl; charset=utf-8");
-  res.redirect(
-    "https://raw.githubusercontent.com/ROHIT12-bit/Sonyyaytelugu/main/jakiekosam.m3u"
-  );
-}
